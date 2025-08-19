695a0523b372533faec04d8a3e11c46d02f8e42b
lara-munro <79603681+lara-munro@users.noreply.github.com>
2025-08-19 13:03:57 -0400

    Barometric pressure aggregation. Updated file names and description. Include data from 2025

--- HOBO_pres_data_agregation.R
+++ HOBO_pres_data_agregation.R
@@ -1,4 +1,8 @@
 # HOBO data aggregation
+# Script aggregates HOBO stage data for 2025 for CLGB.Ag and CLGB.Up
+# Script also aggregates barometric data from WHB and the local barometer installed at CLGB.Ag
+# Script also combines raw stage pressure with barometric pressure to calculate raw water depth (this is also done in individual stage/discharge scripts)
+# This is NOT where you calculate discharge or include the offsets with the visual stage
 
 
 # Load relevant libraries
@@ -11,9 +15,50 @@ dataloc <- "C:/Users/laram/OneDrive - USNH/CLGB/"
 
 # CLGB.Ag stage -----------------------------------------------------------
 
-# Read tables from 2024 for black body HOBO
-staloc <- paste0(dataloc, "CLGB.AG/data/rawData/y2024/Stage/")
-flist <- list.files(staloc, pattern = "*_black.csv")
+# Read tables from 2025 for silver body HOBO
+staloc <- paste0(dataloc, "CLGB.AG/data/stage/y2025/rawData/")
+flist <- list.files(staloc, pattern = "*_silver.csv")
+flist
+
+dfloc <- paste0(staloc, flist[1])
+sta <- read.csv(dfloc, skip = 1)
+sta <- sta[2:4]
+sta <- sta %>%
+  rename(DateTime = Date.Time..GMT.05.00,
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..1303192..SEN.S.N..1303192.,
+         temp.C = Temp...C..LGR.S.N..1303192..SEN.S.N..1303192.)
+sta$DateTime <- as.POSIXct(sta$DateTime, tz = "EST",
+                           format = "%m/%d/%y %I:%M:%S %p")
+sta <- na.omit(sta)
+
+dfloc <- paste0(staloc, flist[4])
+sta2 <- read.csv(dfloc, skip = 1)
+sta2 <- sta2[2:4]
+sta2 <- sta2 %>%
+  rename(DateTime = Date.Time..GMT.05.00,
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..1303192..SEN.S.N..1303192.,
+         temp.C = Temp...C..LGR.S.N..1303192..SEN.S.N..1303192.)
+sta2$DateTime <- as.POSIXct(sta2$DateTime, tz = "EST",
+                            format = "%m/%d/%y %I:%M:%S %p")
+sta2 <- na.omit(sta2)
+
+# merge data frames
+
+sta <- rbind(sta, sta2)
+
+# Remove duplicated rows
+sta <- sta[!duplicated(sta),]
+
+sta$DateTime <- as.character(format(sta$DateTime))
+write.csv(sta, paste0(dataloc,"/CLGB.AG/data/stage/y2025/intermediateData/CLGB.Ag_waterPres2025_silver.csv", row.names = F))
+
+
+
+# CLGB.Up stage -----------------------------------------------------------
+
+# Read tables from 2025 for black body HOBO
+staloc <- paste0(dataloc, "CLGB.UP/stage/y2025/rawData/")
+flist <- list.files(staloc, pattern = "*.csv")
 flist
 
 dfloc <- paste0(staloc, flist[1])
@@ -32,80 +77,51 @@ sta2 <- read.csv(dfloc, skip = 1)
 sta2 <- sta2[2:4]
 sta2 <- sta2 %>%
   rename(DateTime = Date.Time..GMT.05.00,
-         pres.kpa = Abs.Pres..kPa..LGR.S.N..22063078_duplicate..SEN.S.N..22063078.,
-         temp.C = Temp...C..LGR.S.N..22063078_duplicate..SEN.S.N..22063078.)
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..22063078..SEN.S.N..22063078.,
+         temp.C = Temp...C..LGR.S.N..22063078..SEN.S.N..22063078.)
 sta2$DateTime <- as.POSIXct(sta2$DateTime, tz = "EST",
                             format = "%m/%d/%y %I:%M:%S %p")
 sta2 <- na.omit(sta2)
-
-
-# merge data frames
-
 sta <- rbind(sta, sta2)
-
-# Remove duplicated rows
-sta <- sta[!duplicated(sta),]
-
-write.csv(sta, paste0(dataloc,"/CLGB.AG/data/transformedData/y2024/CLGB.Ag_waterPres.csv"))
-
-
-
-# CLGB.Up stage -----------------------------------------------------------
-
-# Read tables from 2024 for black body HOBO
-staloc <- paste0(dataloc, "CLGB.UP/rawData/y2024/Stage/")
-flist <- list.files(staloc, pattern = "*.csv")
-flist
-
-dfloc <- paste0(staloc, flist[1])
-sta <- read.csv(dfloc, skip = 1)
-sta <- sta[2:4]
-sta <- sta %>%
-  rename(DateTime = Date.Time..GMT.05.00,
-         pres.psi = Abs.Pres..psi..LGR.S.N..20434532..SEN.S.N..20434532.,
-         temp.C = Temp...C..LGR.S.N..20434532..SEN.S.N..20434532.)
-sta$pres.kpa <- 6.89476 * sta$pres.psi
-sta$DateTime <- as.POSIXct(sta$DateTime, tz = "EST",
-                           format = "%m/%d/%y %I:%M:%S %p")
-sta <- na.omit(sta)
-
-dfloc <- paste0(staloc, flist[2])
-sta2 <- read.csv(dfloc, skip = 1)
-sta2 <- sta2[2:4]
-sta2 <- sta2 %>%
-  rename(DateTime = Date.Time..GMT.05.00,
-         pres.psi = Abs.Pres..psi..LGR.S.N..20434532..SEN.S.N..20434532.,
-         temp.C = Temp...C..LGR.S.N..20434532..SEN.S.N..20434532.)
-sta2$pres.kpa <- 6.89476 * sta2$pres.psi
-sta2$DateTime <- as.POSIXct(sta2$DateTime, tz = "EST",
-                            format = "%m/%d/%y %I:%M:%S %p")
-sta2 <- na.omit(sta2)
 
 dfloc <- paste0(staloc, flist[3])
 sta3 <- read.csv(dfloc, skip = 1)
 sta3 <- sta3[2:4]
 sta3 <- sta3 %>%
   rename(DateTime = Date.Time..GMT.05.00,
-         pres.psi = Abs.Pres..psi..LGR.S.N..20434532..SEN.S.N..20434532.,
-         temp.C = Temp...C..LGR.S.N..20434532..SEN.S.N..20434532.)
-sta3$pres.kpa <- 6.89476 * sta3$pres.psi
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..22063078..SEN.S.N..22063078.,
+         temp.C = Temp...C..LGR.S.N..22063078..SEN.S.N..22063078.)
 sta3$DateTime <- as.POSIXct(sta3$DateTime, tz = "EST",
                             format = "%m/%d/%y %I:%M:%S %p")
 sta3 <- na.omit(sta3)
 
 # merge data frames
-
 sta <- rbind(sta, sta3)
+
+dfloc <- paste0(staloc, flist[4])
+sta4 <- read.csv(dfloc, skip = 1)
+sta4 <- sta4[2:4]
+sta4 <- sta4 %>%
+  rename(DateTime = Date.Time..GMT.05.00,
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..22063078..SEN.S.N..22063078.,
+         temp.C = Temp...C..LGR.S.N..22063078..SEN.S.N..22063078.)
+sta4$DateTime <- as.POSIXct(sta4$DateTime, tz = "EST",
+                            format = "%m/%d/%y %I:%M:%S %p")
+sta4 <- na.omit(sta4)
+
+# merge data frames
+sta <- rbind(sta, sta4)
 
 # Remove duplicated rows
 sta <- sta[!duplicated(sta),]
 
-write.csv(sta, paste0(dataloc,"CLGB.UP/transformedData/y2024/CLGB.UP_waterPres.csv"))
+sta$DateTime <- as.character(format(sta$DateTime))
+write.csv(sta, paste0(dataloc,"CLGB.UP/stage/y2025/intermediateData/CLGB.UP_waterPres2025.csv"))
 
 
 
 
-# Aggregate barometric data -----------------------------------------------
+# Aggregate 2024 barometric data -----------------------------------------------
 #Aggregate barometric data (not necessarily needed)
 partbaroloc <- paste(dataloc, "WHB Barometric/", sep = "")
 baro2024 <- read.csv(paste(partbaroloc, "annual/WHB_atm_Pressure2024.csv", sep = ""))
@@ -217,13 +233,109 @@ baro2024$DateTime <- as.character(format(baro2024$DateTime))
 write.csv(baro2024, paste0(partbaroloc, "annual/WHB_atm_Pressure2024.csv"), row.names = F)
 
 
+# Aggregate 2025 barometric pressure --------------------------------------
+
+partbaroloc <- paste(dataloc, "WHB Barometric/", sep = "")
+baro2025 <- read.csv(paste(partbaroloc, "annual/WHB_atm_Pressure2025.csv", sep = ""))
+baro2025$DateTime <- as.POSIXct(baro2025$DateTime, tz = "EST",
+                                format = "%Y-%m-%d %H:%M")
+baro2025 <- na.omit(baro2025)
+#baro2025 <- baro2025[3:5]
+
+part1 <- read.csv(paste0(partbaroloc, "WHB_B_20241219to20250204.csv"), skip = 1)
+head(part1)
+part1 <- part1[1:4]
+part1 <- part1 %>%
+  rename(rownb = X.,
+         datetime = Date.Time..GMT.05.00,
+         pres.psi = Abs.Pres..psi..LGR.S.N..10012043..SEN.S.N..10012043.,
+         temp.F = Temp...F..LGR.S.N..10012043..SEN.S.N..10012043.)
+part1$pres.kpa <- 6.89476 * part1$pres.psi
+part1$temp.C <- (part1$temp.F - 32) *5/9
+part1$DateTime <- as.POSIXct(part1$datetime, tz = "EST",
+                             format = "%m/%d/%y %I:%M:%S %p")
+part1 <- part1[5:7]
+
+baro2025 <- rbind(baro2025, part1)
+baro2025 <- baro2025[!duplicated(baro2025),]
+
+part2 <- read.csv(paste0(partbaroloc, "WHB_B_20250204to20250325.csv"), skip = 1)
+head(part2)
+part2 <- part2[1:4]
+part2 <- part2 %>%
+  rename(rownb = X.,
+         datetime = Date.Time..GMT.05.00,
+         pres.psi = Abs.Pres..psi..LGR.S.N..10012043..SEN.S.N..10012043.,
+         temp.F = Temp...F..LGR.S.N..10012043..SEN.S.N..10012043.)
+part2$pres.kpa <- 6.89476 * part2$pres.psi
+part2$temp.C <- (part2$temp.F - 32) *5/9
+part2$DateTime <- as.POSIXct(part2$datetime, tz = "EST",
+                             format = "%m/%d/%Y %H:%M")
+part2 <- part2[5:7]
+
+baro2025 <- rbind(baro2025, part2)
+baro2025 <- baro2025[!duplicated(baro2025),]
+
+
+part3 <- read.csv(paste0(partbaroloc, "WHB_B_20250325to20250501.csv"), skip = 1)
+head(part3)
+part3 <- part3[1:4]
+part3 <- part3 %>%
+  rename(rownb = X.,
+         datetime = Date.Time..GMT.05.00,
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..10012043..SEN.S.N..10012043.,
+         temp.C = Temp...C..LGR.S.N..10012043..SEN.S.N..10012043.)
+part3$DateTime <- as.POSIXct(part3$datetime, tz = "EST",
+                             format = "%m/%d/%y %I:%M:%S %p")
+part3 <- part3[3:5]
+
+baro2025 <- rbind(baro2025, part3)
+baro2025 <- baro2025[!duplicated(baro2025),]
+
+part4 <- read.csv(paste0(partbaroloc, "WHB_B_20250501to20250602.csv"), skip = 1)
+head(part4)
+part4 <- part4[1:4]
+part4 <- part4 %>%
+  rename(rownb = X.,
+         datetime = Date.Time..GMT.05.00,
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..10012043..SEN.S.N..10012043.,
+         temp.C = Temp...C..LGR.S.N..10012043..SEN.S.N..10012043.)
+part4$DateTime <- as.POSIXct(part4$datetime, tz = "EST",
+                             format = "%m/%d/%y %I:%M:%S %p")
+part4 <- part4[3:5]
+
+baro2025 <- rbind(baro2025, part4)
+baro2025 <- baro2025[!duplicated(baro2025),]
+
+# Read baro data from local logger (not WHB)
+
+part5 <- read.csv(paste0(partbaroloc, "baro_CLGB_2025_05-to_2025_07_14.csv"), skip = 1)
+head(part5)
+part5 <- part5[1:4]
+part5 <- part5 %>%
+  rename(rownb = X.,
+         datetime = Date.Time..GMT.05.00,
+         pres.kpa = Abs.Pres..kPa..LGR.S.N..22254013..SEN.S.N..22254013.,
+         temp.C = Temp...C..LGR.S.N..22254013..SEN.S.N..22254013.)
+part5$DateTime <- as.POSIXct(part5$datetime, tz = "EST",
+                             format = "%m/%d/%y %I:%M:%S %p")
+part5 <- part5[3:5]
+
+baro2025 <- rbind(baro2025, part5)
+baro2025 <- baro2025[!duplicated(baro2025),]
+
+
+baro2025$DateTime <- as.character(format(baro2025$DateTime))
+write.csv(baro2025, paste0(partbaroloc, "annual/atm_Pressure2025.csv"), row.names = F)
+
+
 # Read annual barometric data ---------------------------------------------
 
 
 # Read barometrice pressure data
 # Get this data from Jody Potter at WQAL, make sure that units are the same 
 # This data includes 2022 and 2023 in the same data file. It has been included in the 2022 and 2023 baro folder.
-baroloc <- paste(dataloc, "WHB Barometric/annual/WHB_atm_Pressure2024.csv", sep = "")
+baroloc <- paste(dataloc, "WHB Barometric/annual/atm_Pressure2025.csv", sep = "")
 baro <- read.csv(baroloc)
 baro$DateTime <- as.POSIXct(baro$DateTime, tz = "EST",
                                 format = "%Y-%m-%d %H:%M")
@@ -253,25 +365,13 @@ PSI_TO_PSF = 144.0
 
 stage$depth.m = (FEET_TO_METERS) * (KPA_TO_PSI * PSI_TO_PSF * stage$watPres.kPa) / stage$density.lbft
 
-stage$DateTime <- as.character(format(stage$DateTime))
-write.csv(stage, paste0(dataloc,"/CLGB.UP/ratingCurve/CLGB.UP_2024_waterDepth.csv"), row.names = F)
-
-# Covert depth to density
-# Rating curve is in its own Excel file
-# This equation needs to be changed for each site based on the rating curve
-
-stage$discharge.m3s <- 0.0478*(stage$depth.m^1.7148)
-
 # Remane columns with awkward names
-
 stage <- stage %>%
-  rename( TempWat.C.stage = Temp.C.x,
-          TempAir.C = Temp.C.y,
-          AbsPresAir.kPa = AbsPres.kPa,
-          AbsPresWat.psi = AbsPres.psi)
+  rename( TempWat.C.stage = temp.C.x,
+          TempAir.C = temp.C.y,
+          AbsPresAir.kPa = pres.kpa.y,
+          AbsPresWat.kPa = pres.kpa.x)
 
-stage$DateTime <- as.character(format(stage$DateTime))
-
-saveloc <- paste(dataloc, "transformedData/CLGBag_DISCHARGE_2022-2023.csv", sep = "")
-write.csv(stage, saveloc, row.names = F)
-
+stage2 <- stage
+stage2$DateTime <- as.character(format(stage2$DateTime))
+write.csv(stage2, paste0(dataloc,"/CLGB.Up/ratingCurve/CLGB.Up_2025_waterDepth.csv"), row.names = F)
